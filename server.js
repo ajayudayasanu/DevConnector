c2bda3c32c78e5ce2a61d207898b8f980f62f3cb
Ajay U <ajayudayasanu95@gmail.com>
2019-09-08 20:55:26 +0530

    inital commit

---
+++ server.js
@@ -0,0 +1,5 @@
+const express = require('express');
+const app = express();
+app.get('/', (req, res) => res.send('API is Running'));
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`server started on ${PORT}`));
