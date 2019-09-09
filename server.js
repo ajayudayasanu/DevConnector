ce5ac78b70e273147a0fc856fcb44d2eb8018756
Ajay U <ajayudayasanu95@gmail.com>
2019-09-09 13:09:07 +0530

    MongoDB added

--- server.js
+++ server.js
@@ -1,5 +1,16 @@
 const express = require('express');
+const connectDB = require('./config/db');
+
 const app = express();
+//Connect DB
+connectDB();
+
 app.get('/', (req, res) => res.send('API is Running'));
+
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/posts', require('./routes/api/posts'));
+app.use('/api/profile', require('./routes/api/profile'));
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`server started on ${PORT}`));
