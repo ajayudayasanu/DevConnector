be1cc909e15d9a917928bf3142c0b62c5f0f7f81
Ajay U <ajayudayasanu95@gmail.com>
2019-11-04 22:44:44 +0530

    add post route added

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,9 +1,46 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
+const { check, validationResult } = require('express-validator/check');
+const auth = require('../../middleware/auth');
 
-//@route    GET api/posts
-//@desc     test route
-//@access   public
+const Post = require('../../models/Post');
+const Profile = require('../../models/Profile');
+const User = require('../../models/User');
 
-router.get('/', (req, res) => res.send('posts route'));
+//@route    POST api/posts
+//@desc     Adding post
+//@access   Private
+
+router.post(
+  '/',
+  [
+    auth,
+    [
+      check('text', 'text is required')
+        .not()
+        .isEmpty()
+    ]
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    try {
+      const user = await User.findById(req.user.id).select('-password');
+      const newpost = new Post({
+        text: req.body.text,
+        name: user.name,
+        avatar: user.avatar,
+        user: req.user.id
+      });
+      const post = await newpost.save();
+      res.json(post);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('server error');
+    }
+  }
+);
 module.exports = router;
